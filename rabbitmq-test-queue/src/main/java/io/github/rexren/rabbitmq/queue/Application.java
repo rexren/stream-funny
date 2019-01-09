cdf98cf171b576560cf9db99a9a1965f4854d1e2
rensu <>
2019-01-09 23:30:24 +0800

    add test.

--- rabbitmq-test-queue/src/main/java/io/github/rexren/rabbitmq/queue/Application.java
+++ rabbitmq-test-queue/src/main/java/io/github/rexren/rabbitmq/queue/Application.java
@@ -1,13 +1,13 @@
 package io.github.rexren.rabbitmq.queue;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRabbit
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
